2f0ed84f4ce30bc9e6dad6544b2708d365468dcc
starkMrSnow <146622700+starkMrSnow@users.noreply.github.com>
2024-08-25 20:26:45 +0330

    Completed 80% of the intro landing page

--- mevin-web/src/components/Landing/Landing.js
+++ mevin-web/src/components/Landing/Landing.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import './LandingStyles.css'
+import applestore from '../../Assets/AppleStore.png'
+import emoji from '../../Assets/emoji.png'
+import loversPic from '../../Assets/lovers.png'
+import phoneAd from '../../Assets/phone.png'
+import playstore from '../../Assets/playstore.png'
 
- function Landing() {
+function Landing() {
   return (
-    <div>Landing</div>
+    <div className='intro'>
+        <h1>From matches to Marriage:</h1>
+        <h1 style={{ marginLeft: '7vw' }}>Your success story awaits</h1>
+        <div className="rectangle">
+            <p className='paragraph'>Harness the potential at your fingers: Download our mobile app and embark on your dating journey today</p>
+           <div>
+           <img src={playstore} alt="download" className="playstore" />
+           <img src={applestore} alt="download" className="applestore" />
+           </div>
+
+            <img src={loversPic} alt="lover picture" className="lovers" />   
+        </div>
+        <img src={phoneAd} alt="ad" className="phonead" />   
+    </div>
   )
 }
+
 export default Landing
