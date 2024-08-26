7aee83a9cce23519bbe219c85ace1f14bd6d79b0
starkMrSnow <146622700+starkMrSnow@users.noreply.github.com>
2024-08-26 13:03:00 +0330

    a  little changes to the intro part

--- mevin-web/src/components/Landing/Landing.js
+++ mevin-web/src/components/Landing/Landing.js
@@ -1,26 +1,46 @@
 import React from 'react'
 import './LandingStyles.css'
-import applestore from '../../Assets/AppleStore.png'
+import applestore from '../../Assets/Applestore.png'
 import emoji from '../../Assets/emoji.png'
 import loversPic from '../../Assets/lovers.png'
 import phoneAd from '../../Assets/phone.png'
-import playstore from '../../Assets/playstore.png'
+import playstore from '../../Assets/Playstore (2).png'
 
 function Landing() {
   return (
     <div className='intro'>
-        <h1>From matches to Marriage:</h1>
-        <h1 style={{ marginLeft: '7vw' }}>Your success story awaits</h1>
-        <div className="rectangle">
-            <p className='paragraph'>Harness the potential at your fingers: Download our mobile app and embark on your dating journey today</p>
-           <div>
-           <img src={playstore} alt="download" className="playstore" />
-           <img src={applestore} alt="download" className="applestore" />
-           </div>
-
-            <img src={loversPic} alt="lover picture" className="lovers" />   
+      <h1>From matches to Marriage:</h1>
+      <h1 style={{ marginLeft: '7vw' }}>Your success story awaits</h1>
+      <div className="rectangle">
+        <div>
+          <p className='paragraph Harnes-p'>
+            Harness the potential at your fingers: Download our mobile app and embark on your dating journey today
+          </p>
+          <div className="sep">
+            <div className='blackbox'>
+              <img src={playstore} alt="Google PlayStore" />  
+              <div className="getOn">
+                <p>GET ON</p> 
+                <p>Google PlayStore</p>
+              </div>
+            </div>
+            <div className="blackbox">
+              <img src={applestore} alt="Apple Store" /> 
+              <div className="getOn">
+                <p>Download on</p> 
+                <p>Apple Store</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <img src={phoneAd} alt="ad" className="phonead" />   
+        <img src={loversPic} alt="lover picture" className="lovers" /> 
+          <div className="Emoji">
+          <img src={emoji} alt="ad" className="emoji" />  
+            </div> 
+      </div>
+      <img src={phoneAd} alt="ad" className="phonead" /> 
+
+       
     </div>
   )
 }
